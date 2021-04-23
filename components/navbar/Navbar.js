30585b8c119d30077e1b97005db055e4d8ad0ad8
Marko Jovanovic <marko.jovanovic2401@gmail.com>
2021-04-23 17:16:22 +0200

    Implement navigation toolbar
    - Use material UI as a component library
    - Use SASS for styling

---
+++ components/navbar/Navbar.js
@@ -0,0 +1,31 @@
+import Link from 'next/link';
+import { AppBar, Button, IconButton, Toolbar, Typography } from "@material-ui/core";
+import CollectionsIcon from '@material-ui/icons/Collections';
+import styles from './Navbar.module.scss'
+
+export default function Navbar(props) {
+  return (
+    <AppBar className={styles['app-navigation']} position="static">
+      <Toolbar className='toolbar'>
+        <Link href='/'>
+          <a className={styles.brand}>
+            <IconButton edge="start" color="inherit" aria-label="menu">
+              <CollectionsIcon />
+            </IconButton>
+            <Typography variant="h6">
+              Slider App
+            </Typography>
+          </a>
+        </Link>
+
+        <div className={styles.actions}>
+          <Button color="inherit">
+            <Link href='/settings'>
+              <a>Settings</a>
+            </Link>
+          </Button>
+        </div>
+      </Toolbar>
+    </AppBar>
+  );
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
-import '../styles/globals.css'
+import { createMuiTheme, ThemeProvider } from "@material-ui/core";
+import { pink, teal } from "@material-ui/core/colors";
+import Navbar from "../components/navbar/Navbar";
+import '../scss/globals.scss';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: teal[500]
+    },
+    secondary: {
+      main: pink[500]
+    }
+  }
+})
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Navbar />
+
+      <div className='app'>
+        <Component {...pageProps} />
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default MyApp
